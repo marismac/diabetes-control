eeb2903f72087c723ab4b6724735d8ac8170712e
leo.alvesneuwald@gmail.com <leo.alvesneuwald@gmail.com@1bea4743-90e7-56ae-e510-3afb3609e6b0>
2012-04-01 17:25:10 +0000

    01/04/2012 - Ajuste de Pacotes

---
+++ trunk/DiabetesControl/src/com/android/diabetescontrol/database/BasicoDAO.java
@@ -0,0 +1,47 @@
+package com.android.diabetescontrol.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class BasicoDAO {
+
+	protected static SQLiteDatabase mDb;
+	protected static Context context;
+	private static ContextoDados dbAdapter;
+
+	private static int contador;
+
+	public BasicoDAO(Context ctx) {
+		context = ctx;
+	}
+
+	private synchronized static int numeroConexoes(int i) {
+		contador = contador + i;
+		return contador;
+	}
+
+	/**
+	 * Utiliza ctx para instanciar uma base de dados. Abre o banco!
+	 */
+	public synchronized void open() {
+		if (mDb == null || (mDb != null && !mDb.isOpen())) {
+			dbAdapter = new ContextoDados(context);
+			mDb = dbAdapter.open();
+		}
+		numeroConexoes(+1);
+
+	}
+
+	/**
+	 * Fecha o acesso a uma base de dados.
+	 */
+	public synchronized void close() {
+		if (mDb != null && mDb.isOpen() && (numeroConexoes(0) == 1)) {
+			dbAdapter.close();
+			mDb.close();
+		}
+		numeroConexoes(-1);
+
+	}
+
+}
