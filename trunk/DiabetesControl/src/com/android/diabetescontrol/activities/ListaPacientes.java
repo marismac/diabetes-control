5df2a4d01e63fb3c47dc9a043dd525c37cbfb9b8
leo.alvesneuwald@gmail.com <leo.alvesneuwald@gmail.com@1bea4743-90e7-56ae-e510-3afb3609e6b0>
2012-05-13 17:51:54 +0000

    13/05/2012

--- trunk/DiabetesControl/src/com/android/diabetescontrol/activities/ListaPacientes.java
+++ trunk/DiabetesControl/src/com/android/diabetescontrol/activities/ListaPacientes.java
@@ -1,37 +1,24 @@
 package com.android.diabetescontrol.activities;
 
-import java.util.List;
-import java.util.Map;
-
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
+import com.android.diabetescontrol.adapters.PacienteAdapter;
 import com.android.diabetescontrol.business.PacientesBusiness;
 
 public class ListaPacientes extends ListActivity {
-	private TextView textSem;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.listapaciente);
+		setListAdapter(new PacienteAdapter(this, new PacientesBusiness().getPacientesDoMedico(this)));
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		l.getAdapter().getItem(position);
 
-		List<Map<String, String>> regs = new PacientesBusiness()
-				.getPacientesDoMedico(this);
-		if (regs != null && !regs.isEmpty()) {
-			textSem = (TextView) findViewById(R.id.tvSemPac);
-			textSem.setVisibility(textSem.INVISIBLE);
-			String[] from = { "Master", "Detail" };
-			int[] to = { android.R.id.text1, android.R.id.text2 };
-
-			SimpleAdapter ad = new SimpleAdapter(this, regs,
-					android.R.layout.simple_list_item_2, from, to);
-			ListView lv = (ListView) findViewById(android.R.id.list);
-			lv.setAdapter(ad);
-			lv.setCacheColorHint(0);
-		}
+		super.onListItemClick(l, v, position, id);
 	}
 }
--- trunk/DiabetesControl/src/com/android/diabetescontrol/business/PacientesBusiness.java
+++ trunk/DiabetesControl/src/com/android/diabetescontrol/business/PacientesBusiness.java
@@ -1,9 +1,7 @@
 package com.android.diabetescontrol.business;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -13,23 +11,18 @@ import com.android.diabetescontrol.model.Paciente;
 
 public class PacientesBusiness {
 	@SuppressWarnings("static-access")
-	public List<Map<String, String>> getPacientesDoMedico(Context context) {
-		List<Map<String, String>> l = new ArrayList<Map<String, String>>();
+	public List<Paciente> getPacientesDoMedico(Context context) {
+		List<Paciente> lista = new ArrayList<Paciente>();
 		PacienteDAO pacDAO = new PacienteDAO(context);
-		Map<String, String> m = null;
 		pacDAO.open();
 		Cursor c = pacDAO.consultarPacientesWhereOrder(pacDAO.COLUNA_EDITAR
 				+ " == 'N'", pacDAO.COLUNA_NOME);
 		c.moveToFirst();
 		while (!c.isAfterLast()) {
-			m = new HashMap<String, String>();
-			Paciente paciente = pacDAO.deCursorParaPaciente(c);
-			m.put("Master", paciente.getNome() + " (" + paciente.getId() + ")");
-			m.put("Detail", paciente.getEmail());
-			l.add(m);
+			lista.add(pacDAO.deCursorParaPaciente(c));
 			c.moveToNext();
 		}
 		pacDAO.close();
-		return l;
+		return lista;
 	}
 }
