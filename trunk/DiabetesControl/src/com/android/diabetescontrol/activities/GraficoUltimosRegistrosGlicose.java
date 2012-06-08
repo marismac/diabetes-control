3d35baba5a994798ec7117f5ce13370e816b8672
leo.alvesneuwald@gmail.com <leo.alvesneuwald@gmail.com@1bea4743-90e7-56ae-e510-3afb3609e6b0>
2012-06-08 02:49:00 +0000

    07/06/2012

---
+++ trunk/DiabetesControl/src/com/android/diabetescontrol/activities/GraficoUltimosRegistrosGlicose.java
@@ -0,0 +1,79 @@
+package com.android.diabetescontrol.activities;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+
+import com.android.diabetescontrol.database.RegistroDAO;
+import com.android.diabetescontrol.util.Constante;
+
+public class GraficoUltimosRegistrosGlicose {
+	public Intent getIntent(Context context) {
+		int[] y = getValores(context);
+		int[] x = new int[y.length];
+		for (int i = 0; i < y.length; i++) {
+			x[i] = i;
+		}
+		XYSeries series = new XYSeries("Registros");
+		for (int i = 0; i < x.length; i++) {
+			series.add(x[i], y[i]);
+		}
+		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		dataset.addSeries(series);
+		XYSeriesRenderer renderer = new XYSeriesRenderer();
+		renderer.setPointStyle(PointStyle.TRIANGLE);
+		renderer.setLineWidth(6);
+		renderer.setColor(Color.BLACK);
+		renderer.setDisplayChartValues(true);
+		XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+		mRenderer.addSeriesRenderer(renderer);
+		mRenderer.setChartTitle("Registros de Glicose");
+		mRenderer.setXTitle("Registros");
+		mRenderer.setYTitle("Valor");
+		mRenderer.setZoomButtonsVisible(true);
+		mRenderer.setApplyBackgroundColor(true);
+		mRenderer.setBackgroundColor(Color.parseColor("#F5F5F5"));
+		mRenderer.setMarginsColor(Color.parseColor("#F5F5F5"));
+		mRenderer.setLabelsColor(Color.BLACK);
+		mRenderer.setXLabelsColor(Color.BLACK);
+		Intent intent = ChartFactory.getScatterChartIntent(context, dataset,
+				mRenderer);
+		return intent;
+	}
+
+	public int[] getValores(Context ctx) {
+		RegistroDAO regDao = new RegistroDAO(ctx);
+		String sql = getSql(regDao);
+		regDao.open();
+		Cursor c = regDao.consultar(sql);
+		c.moveToFirst();
+		Float valor;
+		int valores[] = new int[c.getCount()];
+		int i = 0;
+		while (!c.isAfterLast()) {
+			valor = c.getFloat(c.getColumnIndex(c.getColumnName(0)));
+			c.moveToNext();
+			valores[i] = valor.intValue();
+			i++;
+		}
+		return valores;
+	}
+
+	private String getSql(RegistroDAO regDao) {
+		@SuppressWarnings("static-access")
+		String sql = "SELECT " + regDao.COLUNA_VALOR + ", "
+				+ regDao.COLUNA_TIPO + ", " + regDao.COLUNA_UNIDADE + " FROM "
+				+ regDao.TABELA_REGISTRO + " WHERE " + regDao.COLUNA_TIPO
+				+ " = '" + Constante.TIPO_GLICOSE + "' ORDER BY "
+				+ regDao.COLUNA_DATAHORA + " ASC";
+		return sql;
+	}
+}
--- trunk/DiabetesControl/src/com/android/diabetescontrol/util/Constante.java
+++ trunk/DiabetesControl/src/com/android/diabetescontrol/util/Constante.java
@@ -1,5 +1,17 @@
 package com.android.diabetescontrol.util;
 
-public class Constante {
+public final class Constante {
 	public static final String TIPO_GLICOSE = "Glicose";
+	public static final String TIPO_PRESSAO = "Pressão";
+	public static final String TIPO_HBA1C = "HbA1c";
+	public static final String TIPO_PESO = "Peso";
+	public static final String TIPO_GORDURA = "Gordura";
+	public static final String CAT_A_CAFE = "Antes do Café";
+	public static final String CAT_D_CAFE ="Depois do Café";
+	public static final String CAT_A_ALMOCO ="Antes do Almoço";
+	public static final String CAT_D_ALMOCO ="Depois da Almoço";
+	public static final String CAT_A_LANCHE ="Antes do Lanche";
+	public static final String CAT_D_LANCHE ="Depois do Lanche";
+	public static final String CAT_A_JANTA ="Antes da Janta";
+	public static final String CAT_D_JANTA ="Depois da Janta";
 }
